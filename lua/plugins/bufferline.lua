63a2ebe3623cb91d47224970a1cb45f86800ac69
Bryce Martin <brycem0418@gmail.com>
2026-02-04 08:55:35 -0600

    Add bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,13 @@
+return
+    -- In your plugins list
+{
+  'akinsho/bufferline.nvim',
+  version = "*",
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    require("bufferline").setup({})
+  local map = vim.keymap.set
+  map('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', { desc = 'Next Buffer' })
+  map('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', { desc = 'Prev Buffer' })
+  end
+}
