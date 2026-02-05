07f1e331b7124c43eed14d50c4283a8a0436a258
Bryce Martin <brycem0418@gmail.com>
2026-02-04 18:04:37 -0600

    remove none-ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,6 +22,9 @@ return {
             vim.lsp.enable('clangd', {
                 capabilities = capabilities
             })
+            vim.lsp.enable('lua_ls', {
+                capabilities = capabilities
+            })
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
             vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
