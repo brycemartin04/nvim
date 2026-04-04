3f69cc47e82d62144d7c9f9a2b062a1bf24f4fe8
Bryce Martin <brycem0418@gmail.com>
2026-04-04 01:26:34 +0000

    tab completions

--- init.lua
+++ init.lua
@@ -20,7 +20,12 @@ vim.opt.listchars = {
   nbsp = "⣿",
 }
 
-vim.opt.clipboard = "unnamedplus"
+-- Copy to System Clipboard
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
+
+-- Paste from system clipboard
+vim.keymap.set({'n', 'v'}, '<leader>p', '"+p', { desc = 'Paste from system clipboard' })
+vim.keymap.set({'n', 'v'}, '<leader>P', '"+P', { desc = 'Paste from system clipboard (before)' })
 
 vim.keymap.set("n", "<C-d>","<C-d>_zz")
 vim.keymap.set("n", "<C-u>","<C-u>_zz")
