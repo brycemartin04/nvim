f208e089f9d09bc0db874970f3bd12984c7a37a0
Bryce Martin <brycem0418@gmail.com>
2026-04-04 01:15:08 +0000

    use system clipboard by default

--- init.lua
+++ init.lua
@@ -20,13 +20,7 @@ vim.opt.listchars = {
   nbsp = "⣿",
 }
 
--- Yank into system clipboard
-vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
-vim.keymap.set('n', '<leader>yy', '"+yy', { desc = 'Yank line to system clipboard' })
-
--- Paste from system clipboard
-vim.keymap.set({'n', 'v'}, '<leader>p', '"+p', { desc = 'Paste from system clipboard' })
-vim.keymap.set({'n', 'v'}, '<leader>P', '"+P', { desc = 'Paste from system clipboard (before)' })
+vim.opt.clipboard = "unnamedplus"
 
 vim.keymap.set("n", "<C-d>","<C-d>_zz")
 vim.keymap.set("n", "<C-u>","<C-u>_zz")
